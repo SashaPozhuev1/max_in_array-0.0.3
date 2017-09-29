145e1717cf10a2439bcc073da56a067489d379d4
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2017-09-29 18:42:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 bool proverka( int mass[ 10 ] ) {
-
 	string stroka;
 	getline( cin, stroka );
 	istringstream stream( stroka );
@@ -26,26 +25,14 @@ int main() {
 
 	int mass1[ 10 ];
 	int mass2[ 10 ];
-	int mass3[ 20 ];
 
 	if ( ( proverka( mass1 ) ) && ( proverka( mass2 ) ) ) {
-		for ( int i = 0; i < 20; i++ ) {
-			if ( i % 2 == 0 ) {
-				mass3[ i ] = mass1[ i / 2 ];
-			}
-			else {
-				mass3[ i ] = mass2[ i / 2 ];
-			}
-		}
-
-		int max = mass3[ 0 ] + mass3[ 1 ];
-
-		for ( int i = 0; i < 19; i += 2 ) {
-			if ( max < ( mass3[ i ] + mass3[ i + 1 ] ) ) {
-				max = mass3[ i ] + mass3[ i + 1 ];
-			}
-			if ( max < ( mass3[ i ] + mass3[ i + 3 ] ) ) {
-				max = mass3[ i ] + mass3[ i + 3 ];
+		int max = mass1[ 0 ] + mass2[ 0 ];
+		for ( int i = 0; i < 10; i ++ ) {
+			for ( int j = i; j < 10; j ++ ) {
+				if ( max < mass1[ i ] + mass2[ j ] ) {
+					max = mass1[ i ] + mass2[ j ];
+				}
 			}
 		}
 
