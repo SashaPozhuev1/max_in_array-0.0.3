c9250da38f99895d2151619674549933c51e4205
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2017-09-29 16:59:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-bool proverka(int mass[10]) {
+bool proverka( int mass[ 10 ] ) {
 
 	string stroka;
-	getline(cin, stroka);
-	istringstream stream(stroka);
+	getline( cin, stroka );
+	istringstream stream( stroka );
 
 	bool prav = true;
 
-	for (int i = 0; i < 10; i++) {
-		if (!(stream >> mass[i])) {
+	for ( int i = 0; i < 10; i++ ) {
+		if ( !( stream >> mass[ i ] ) ) {
 			prav = false;
 			break;
 		}
@@ -24,30 +24,30 @@ bool proverka(int mass[10]) {
 
 int main() {
 
-	int mass1[10];
-	int mass2[10];
-	int mass3[20];
+	int mass1[ 10 ];
+	int mass2[ 10 ];
+	int mass3[ 20 ];
 
-	if ((proverka(mass1)) && (proverka(mass2))) {
+	if ( ( proverka( mass1 ) ) && ( proverka( mass2 ) ) ) {
 
-		for (int i = 0; i < 20; i++) {
-			if (i % 2 == 0) {
-				mass3[i] = mass1[i / 2];
+		for ( int i = 0; i < 20; i++ ) {
+			if ( i % 2 == 0 ) {
+				mass3[ i ] = mass1[ i / 2 ];
 			}
 			else {
-				mass3[i] = mass2[i / 2];
+				mass3[ i ] = mass2[ i / 2 ];
 			}
 		}
 
 
-		int max = mass3[0] + mass3[1];
+		int max = mass3[ 0 ] + mass3[ 1 ];
 
-		for (int i = 0; i < 19; i += 2) {
-			if (max < (mass3[i] + mass3[i + 1])) {
-				max = mass3[i] + mass3[i + 1];
+		for ( int i = 0; i < 19; i += 2 ) {
+			if ( max < ( mass3[ i ] + mass3[ i + 1 ] ) ) {
+				max = mass3[ i ] + mass3[ i + 1 ];
 			}
-			if (max < (mass3[i] + mass3[i + 3])) {
-				max = mass3[i] + mass3[i + 3];
+			if ( max < ( mass3[ i ] + mass3[ i + 3 ] ) ) {
+				max = mass3[ i ] + mass3[ i + 3 ];
 			}
 		}
 
